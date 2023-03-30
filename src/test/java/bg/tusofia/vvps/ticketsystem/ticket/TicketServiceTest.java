a0ed1ed412a306eb13b51b4eb794318c1215add2
Lachezar Kolev <76496265+LachezarKolevGit@users.noreply.github.com>
2023-03-30 09:43:34 +0300

    added unit tests

--- src/test/java/bg/tusofia/vvps/ticketsystem/ticket/TicketServiceTest.java
+++ src/test/java/bg/tusofia/vvps/ticketsystem/ticket/TicketServiceTest.java
@@ -4,4 +4,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TicketServiceTest {
 
+    static TicketService ticketService;
+
+    @Mock
+    UserService userService;
+
+    @Mock
+    TrainService trainService;
+
+    @BeforeAll
+    void init(){
+         ticketService = new TicketService(userService, trainService);
+    }
+
+    @DisplayName("Test .calculatePrice() method")
+    @Test
+    void testCalculatePrice(){
+        Train train = new Train();
+        Long trainCarriageId = 1L;
+        Long seatId = 1L;
+        int numberOfTickets = 1;
+        
+        when(userService.getLoggedInUser()).thenReturn(new User("Gosho", 50, true, LocalDate.of(1989, 1, 1)));
+       double actualPrice = ticketService.calculatePrice(train, numberOfTickets, trainCarriageId, seatId);
+       double expectedPrice = ;
+       assertEquals(expectedPrice, actualprice, "ExpectedPrice does not match the actualPrice");
+
+
+    }
+
 }
--- src/test/java/bg/tusofia/vvps/ticketsystem/train/TrainServiceTest.java
+++ src/test/java/bg/tusofia/vvps/ticketsystem/train/TrainServiceTest.java
@@ -25,10 +25,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-//@DataJpaTest
+@DataJpaTest
 class TrainServiceTest {
 
+    private static double DIESEL_PRICE = 2.80;
+
     static TrainService trainService;
+
+    @Mock
+    static RouteService routeService;
+
     @Mock
     static TrainRepository trainRepository;
 
@@ -40,12 +46,16 @@ class TrainServiceTest {
 
     @BeforeAll
     static void init() {
-        trainService = new TrainService(trainRepository, null,trainCarriageRepository, seatRepository );
+        trainService = new TrainService(trainRepository, routeService, trainCarriageRepository, seatRepository );
     }
 
     @DisplayName("Test .calculateBasePrice() method if it returns the correct value")
     @Test
     void testCalculateBasePrice() {
+        *//
+        30 + 30 + 15 = 75 total seats
+        //*
+
         TrainStation sofiaTrainStation = new TrainStation("Sofia Central station", 42.712115, 23.321046);
         TrainStation plovdivTrainStation = new TrainStation("Plovdiv Central railway station", 42.134444, 24.741389);
         TrainStation burgasTrainStation = new TrainStation("Burgas Central railway station", 42.490833, 27.4725);
@@ -65,8 +75,40 @@ class TrainServiceTest {
         Train train = new Train(trainCarriageSet, null, null, route);
         Long trainId = 1L;
         when(trainRepository.findById(trainId)).thenReturn(Optional.of(train));
+        when(routeService.calculateRouteDistance(route)).thenReturn(); //later addd the total distance of the route
+        
+        int distance = routeService.calculateRouteDistance(route);
 
-        trainService.calculateBasePrice(trainId);
+        double actualBasePrice = trainService.calculateBasePrice(trainId);
+        double expectedBasePrice = distance * DIESEL_PRICE / 75;
+        assertEquals(expectedBasePrice, actualBasePrice, "ExpectedBasePrice does not match the actualBasePrice");
+    }
+
+    @DisplayName("Test .getTrainCarriage() method if it returns the correct value")
+    @Test
+    void testGetTrainCarriageClass(){
+        TrainCarriage trainCarriage = new TrainCarriage(TrainCarriageType.CLASS_B,null, 30);
+        Long trainCarriageId = 1L;
+        when(trainCarriageRepository.findById(trainCarriageId)).thenReturn(Optional.of(trainCarriage));
+        double actualMultiplier = trainService.getTrainCarriageClass(trainCarriage.getId(trainCarriageId));
+        double expectedMultiplier = 1;
+        
+        assertEquals(expectedMuilplier, actualMuilplier, "ExpectedMultiplier is not the same as actualMultiplier");
+        
+    }
+
+    @DisplayName("Test .changeSeatStatus() method")
+    @Test
+    void testChangeSeatStatus(){
+        Long seatId = 1L;
+        trainService.changeSeatStatus(seatId);
+       Seat seat = seatRepository.findById(seatId);
+       SeatState actualSeatState = seat.getSeatState();
+       SeatState expectedSeatState = SeatState.SOLD;
+
+       assertEquals(expectedSeatState, actualSeatState, "ExpectedSeatState does not match the actualSeatState");
+
+
     }
 
 
